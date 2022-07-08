56228569c04dccb5900facee322d0a6b2036baff
jiayinliu25 <108898526+jiayinliu25@users.noreply.github.com>
2022-07-08 11:32:29 -0400

    Openlayer

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 import "ol/ol.css";
+import "ol-layerswitcher/dist/ol-layerswitcher.css";
+
 import Map from "ol/Map";
 import OSM from "ol/source/OSM";
 import TileLayer from "ol/layer/Tile";
 import TileWMS from "ol/source/TileWMS";
 import View from "ol/View";
+import Overlay from "ol/Overlay";
+
+import LayerSwitcher from "ol-layerswitcher";
+import { BaseLayerOptions, GroupLayerOptions } from "ol-layerswitcher";
 
 const layers = [
   new TileLayer({
     source: new OSM()
   }),
   new TileLayer({
+    title: "Blocks",
     extent: [-13884991, 2870341, -7455066, 6338219],
     source: new TileWMS({
       url: "http://virtualblackcharlotte.net/geoserver/Charlotte/wms",
@@ -17,6 +24,15 @@ const layers = [
       serverType: "geoserver",
       // Countries have transparency, so do not fade tiles:
       transition: 0
+    })
+  }),
+  new TileLayer({
+    title: "Buildings",
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new TileWMS({
+      url: "http://virtualblackcharlotte.net/geoserver/Charlotte/wms",
+      params: { LAYERS: "Charlotte:Buildings", TILED: true },
+      serverType: "geoserver"
     })
   })
 ];
@@ -28,3 +44,8 @@ const map = new Map({
     zoom: 16
   })
 });
+const layerSwitcher = new LayerSwitcher({
+  reverse: true,
+  groupSelectStyle: "group"
+});
+map.addControl(layerSwitcher);
